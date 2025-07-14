ee8e8acd6cd1b090d33b75c8263f765a32be3a32
AashishBadal <aashishbadal4@gmail.com>
2025-07-14 20:08:00 +0545

    created a job model to store job information and created a authmiddleware to authenticate users with jwt token and added post job controller for company

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -5,6 +5,7 @@ import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import generateToken from "../utils/generateToken.js";
 import connectCloudinary from "../config/cloudinary.js";
+import Job from "../models/job.js";
 
 export const registerCompany = async (req, res) => {
   const { name, email, password } = req.body;
@@ -44,43 +45,36 @@ export const registerCompany = async (req, res) => {
         email: company.email,
         image: company.image,
       },
-      token:generateToken(company._id)
+      token: generateToken(company._id),
     });
   } catch (error) {
-    res.json({success:false,message:error.message})
+    res.json({ success: false, message: error.message });
   }
 };
 
 // company login
 
 export const loginCompany = async (req, res) => {
-
-  const {email,password}= req.body
+  const { email, password } = req.body;
   try {
+    const company = await Company.findOne({ email });
 
-    const company = await Company.findOne({email})
-
-    if(bcrypt.compare(password,company.password)){
+    if (bcrypt.compare(password, company.password)) {
       res.json({
-        success:true,
-        company:{
-          _id:company._id,
-          name:company.name,
-          email:company.name,
-          image:company.image
+        success: true,
+        company: {
+          _id: company._id,
+          name: company.name,
+          email: company.name,
+          image: company.image,
         },
-        token:generateToken(company._id)
-      })
+        token: generateToken(company._id),
+      });
+    } else {
+      res.json({ success: false, message: "Invalid email or password" });
     }
-
-    else{
-      res.json({success:false,message:'Invalid email or password'})
-    }
-
   } catch (error) {
-
-    res.json({success:false,message:error.message})
-    
+    res.json({ success: false, message: error.message });
   }
 };
 
@@ -90,7 +84,31 @@ export const getCompanyData = async (req, res) => {};
 
 // post a new job
 
-export const postJob = async (req, res) => {};
+export const postJob = async (req, res) => {
+  const { title, description, location, salary ,level,category} = req.body;
+
+  const companyId = req.company._id;
+
+  try {
+    const newJob = new Job({
+      title,
+      description,
+      location,
+      salary,
+      companyId,
+      date:Date.now(),
+      level,
+      category
+    })
+    await newJob.save()
+    res.json({success:true,newJob})
+
+  } catch (error) {
+    res.json({success:false,message:error.message})
+  }
+
+
+};
 
 // get company job applicants
 
